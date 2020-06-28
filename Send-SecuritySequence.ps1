ab390d90191ad4e1124d70358948a41118eae4c2
rumrais1n <rumraisin@rumraisin.net>
2020-06-28 19:09:25 +0900

    引数で実行時間を指定できるようにした

--- Send-SecuritySequence.ps1
+++ Send-SecuritySequence.ps1
@@ -1,9 +1,16 @@
+Param(
+	# 引数の指定がなければ5秒待つ設定にする
+	[Int32]$TimeWait = 5
+)
+
 # コンポーネントの読み込み
 Add-Type -AssemblyName Microsoft.VisualBasic
 Add-Type -AssemblyName System.Windows.Forms
 
-# 実行まで一定時間待つ
-Start-Sleep -s 5
-
-# Ctrl+Alt+Endを送る
-[System.Windows.Forms.SendKeys]::SendWait("^%{END}");
+# 実行まで -TimeWait で指定した時間待ってCtrl+Alt+Endを送る
+If ($TimeWait -ge 0) {
+	Write-Host $TimeWait"秒後に Ctrl+Alt+End キーを送信します。"
+	
+	Start-Sleep -s $TimeWait
+	[System.Windows.Forms.SendKeys]::SendWait("^%{END}");
+}
